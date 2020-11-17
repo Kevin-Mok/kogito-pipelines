37120ef0a2e868482322ff03046bba6a13955fcb
Kevin Mok <kmok@redhat.com>
2020-11-16 21:57:38 -0500

    Unit tests for doesBranchExist/addExamplesParams

--- jenkins-tests/JenkinsfilePrBddTests.groovy
+++ jenkins-tests/JenkinsfilePrBddTests.groovy
@@ -7,6 +7,7 @@ class JenkinsfilePrBddTests extends JenkinsPipelineSpecification {
     def params = []
     def changeAuthor = 'user'
     def changeBranch = 'user-branch'
+    def changeTarget = 'user-target'
 
     def setup() {
         Jenkinsfile = loadPipelineScriptForTest('Jenkinsfile.pr-bdd-tests')
@@ -15,6 +16,7 @@ class JenkinsfilePrBddTests extends JenkinsPipelineSpecification {
         Jenkinsfile.getBinding().setVariable('params', params)
         Jenkinsfile.getBinding().setVariable('changeAuthor', changeAuthor)
         Jenkinsfile.getBinding().setVariable('changeBranch', changeBranch)
+        Jenkinsfile.getBinding().setVariable('changeTarget', changeTarget)
     }
 
 	def '[JenkinsfilePrBddTests.groovy] getPRRepoName' () {
@@ -46,23 +48,52 @@ class JenkinsfilePrBddTests extends JenkinsPipelineSpecification {
             0 * getPipelineMock('string.call').call(['name' : 'BUILD_BRANCH_NAME', 'value' : changeBranch])
 	}
 
-	/* def '[JenkinsfilePrBddTests.groovy] addExamplesParamsForOperator: exists' () {
+	def '[JenkinsfilePrBddTests.groovy] doesBranchExist: doesn\'t exist' () {
 		setup:
-            getPipelineMock('githubscm.getRepositoryScm')('kogito-examples', changeAuthor, changeBranch) >> 'repo'
+            getPipelineMock('githubscm.getRepositoryScm')('repo', changeAuthor, changeBranch) >> null
 		when:
-			Jenkinsfile.addExamplesParamsForOperator(params)
+            def repo = Jenkinsfile.doesBranchExist('repo', changeAuthor, changeBranch)
 		then:
-            1 * getPipelineMock('string.call').call(['name' : 'EXAMPLES_URI', 'value' : "https://github.com/${changeAuthor}/kogito-examples"])
-            1 * getPipelineMock('string.call').call(['name' : 'EXAMPLES_REF', 'value' : changeBranch])
+            repo == false
 	}
 
-	def '[JenkinsfilePrBddTests.groovy] addExamplesParamsForOperator: doesn\'t exist' () {
+	def '[JenkinsfilePrBddTests.groovy] doesBranchExist: exists' () {
 		setup:
-            getPipelineMock('githubscm.getRepositoryScm')('kogito-examples', changeAuthor, changeBranch) >> null
+            getPipelineMock('githubscm.getRepositoryScm')('repo', changeAuthor, changeBranch) >> 'repo'
 		when:
-			Jenkinsfile.addExamplesParamsForOperator(params)
+            def repo = Jenkinsfile.doesBranchExist('repo', changeAuthor, changeBranch)
 		then:
+            repo == true
+	}
+
+    def '[JenkinsfilePrBddTests.groovy] addExamplesParams: user branch exists' () {
+        setup:
+            getPipelineMock('githubscm.getRepositoryScm')('kogito-examples', changeAuthor, changeBranch) >> 'repo'
+        when:
+            Jenkinsfile.addExamplesParams(params)
+        then:
+            1 * getPipelineMock('string.call').call(['name' : 'EXAMPLES_URI', 'value' : "https://github.com/${changeAuthor}/kogito-examples"])
+            1 * getPipelineMock('string.call').call(['name' : 'EXAMPLES_REF', 'value' : changeBranch])
+    }
+
+    def '[JenkinsfilePrBddTests.groovy] addExamplesParams: target branch exists' () {
+        setup:
+            getPipelineMock('githubscm.getRepositoryScm')('kogito-examples', changeAuthor, changeBranch) >> null
+            getPipelineMock('githubscm.getRepositoryScm')('kogito-examples', 'kiegroup', changeTarget) >> 'repo'
+        when:
+            Jenkinsfile.addExamplesParams(params)
+        then:
+            1 * getPipelineMock('string.call').call(['name' : 'EXAMPLES_URI', 'value' : 'https://github.com/kiegroup/kogito-examples'])
+            1 * getPipelineMock('string.call').call(['name' : 'EXAMPLES_REF', 'value' : changeTarget])
+    }
+    def '[JenkinsfilePrBddTests.groovy] addExamplesParams: neither exist' () {
+        setup:
+            getPipelineMock('githubscm.getRepositoryScm')('kogito-examples', changeAuthor, changeBranch) >> null
+            getPipelineMock('githubscm.getRepositoryScm')('kogito-examples', 'kiegroup', changeTarget) >> null
+        when:
+            Jenkinsfile.addExamplesParams(params)
+        then:
             1 * getPipelineMock('string.call').call(['name' : 'EXAMPLES_URI', 'value' : 'https://github.com/kiegroup/kogito-examples'])
             1 * getPipelineMock('string.call').call(['name' : 'EXAMPLES_REF', 'value' : 'master'])
-	} */
+    }
 }
